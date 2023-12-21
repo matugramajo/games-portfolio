088ef49aaac67f96b0c955589b458a281883ffee
Matilde Gramajo <matildegramajo@hotmail.com>
2023-12-21 19:09:19 -0300

    add videos

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,20 +1,20 @@
-import { EmailTemplate } from '../../../components/EmailTemplate';
-import { Resend } from 'resend';
+// import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['matildegramajo@hotmail.com'],
-      subject: 'Solicitud de duo nueva',
-      react: EmailTemplate({ name, riotID, steam, serDuo }),
-      html: '',
-    });
+// export async function POST() {
+//   try {
+//     const data = await resend.emails.send({
+//       from: 'Acme <onboarding@resend.dev>',
+//       to: ['matildegramajo@hotmail.com'],
+//       subject: 'Solicitud de duo nueva',
+//       react: EmailTemplate({ name, riotID, steam, serDuo }),
+//       html: '',
+//     });
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error });
+//   }
+// }
